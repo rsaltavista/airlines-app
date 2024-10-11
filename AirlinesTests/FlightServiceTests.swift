3ad66184fe0c35925f3decdcdab8e944af6c808f
Ricardo <rsaltavista@gmail.com>
2024-10-11 17:59:42 -0300

    Create Service tests

---
+++ AirlinesTests/FlightServiceTests.swift
@@ -0,0 +1,35 @@
+//
+//  FlightServiceTests.swift
+//  AirlinesTests
+//
+//  Created by Ricardo Altavista on 11/10/24.
+//
+
+import XCTest
+@testable import Airlines
+
+final class FlightServiceTests: XCTestCase {
+    var service: FlightService?
+    
+    override func setUp() {
+        super.setUp()
+        service = FlightService()
+    }
+    
+    override func tearDown() {
+        service = nil
+        super.tearDown()
+    }
+    
+    func testLoadFlights(){
+        service?.loadFlights(completion: { result in
+            switch result {
+            case .success(let flights):
+                XCTAssertEqual(flights.count, 20)
+            case .failure(let failure):
+                XCTFail("Fail loading flights from json")
+            }
+        })
+    }
+
+}
